9951069f2ccc60df864e334f62845a769301ed39
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-06-08 02:26:26 -0700

    Feat: MongoDB ulanish

--- server.js
+++ server.js
@@ -1,49 +1,29 @@
-console.log("Web serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR", err);
-  } else {
-    user = JSON.parse(data);
+let db;
+const connectionString =
+  "mongodb+srv://abdulboriy6863:Aa1122334455%40@cluster0.94bxndd.mongodb.net/Reja";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifieldTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB ");
+    else {
+      console.log("MongoDB connection succed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-
-//1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//2: Session code
-
-//3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-//4 Routing code
-// app.get("/hello", function (req, res)
-//   res.end("<h1 style = 'background: red'>HELLO WORLD</h1>");
-// });
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-  );
-});
+);
